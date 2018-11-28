8e56f238ad23acd2107966e59ab4256f382779f4
tehnools <noelsoong@live.com>
2018-11-28 20:05:24 +1300

    added statefulness to app

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,18 @@ import 'package:flutter/widgets.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    //todo
+
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
   @override
   Widget build(BuildContext ctx) {
     return MaterialApp(
@@ -11,10 +22,26 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: Text('Foodie'),
       ),
-      body: Card(
-        child: Column(
-          children: <Widget>[Image.asset('assets/food.jpg'), Text('Food Paradies')],
-        ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            child: RaisedButton(
+              child: Text('Add Product'),
+              onPressed: () {},
+            ),
+          ),
+          Column(
+            children: _products.map((element) => Card(
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset('assets/food.jpg'),
+                      Text(element)
+                    ],
+                  ),
+                )).toList(),
+          ),
+        ],
       ),
     ));
   }
