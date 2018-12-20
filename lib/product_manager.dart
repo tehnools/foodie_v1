cd2de3d4bbc2d8ea7916cf292939e66b6a728f33
tehnools <noelsoong@live.com>
2018-12-20 16:32:43 +1300

    :arrow_up: Controller used to raise state up.

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // Local Imports
 import './products.dart';
+import 'product_control.dart';
 
 class ProductManager extends StatefulWidget {
   final String startingProduct;
@@ -26,30 +27,28 @@ class _ProductManagerState extends State<ProductManager> {
     _products.add(widget.startingProduct);
   }
 
+
+  @override
+    void didUpdateWidget(ProductManager oldWidget) {
+      print('Products Manager didUpdateWidget');
+      super.didUpdateWidget(oldWidget);
+    }
+
+    void _addProduct(String productName){
+      setState(() {
+                _products.add(productName);
+              });
+    }
+
   @override
   Widget build(BuildContext context) {
     print('Products Manager Build');
     return Column(children: [
       Container(
         margin: EdgeInsets.all(10),
-        child: RaisedButton(
-          color: Theme.of(context).primaryColor,
-            onPressed: () {
-              setState(() {
-                _products.add('Advanced Food Tester');
-              });
-              print(_products);
-            },
-            child: Text('Add Product')),
+        child: ProductControl(_addProduct),
       ),
       Products(_products)
     ]);
   }
-
-  @override
-    void didUpdateWidget(ProductManager oldWidget) {
-      print('Products Manager didUpdateWidget');
-      // TODO: implement didUpdateWidget
-      super.didUpdateWidget(oldWidget);
-    }
 }
