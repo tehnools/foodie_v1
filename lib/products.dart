1c659dcab5c2298f9b313864cf9b94309fc43782
tehnools <noelsoong@live.com>
2019-01-23 16:08:35 +1300

    more efficient form of listview with a list builder

--- lib/products.dart
+++ lib/products.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
 
 class Products extends StatelessWidget {
-final List<String> products;
+  final List<String> products;
 
-Products([this.products = const []]){
-print('Products Widget Constructor');
-}
+  Products([this.products = const []]) {
+    print('Products Widget Constructor');
+  }
 
-@override
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text(products[index])
+        ],
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     print('Products Widget Build');
-    return  ListView(
-            children: products
-                .map((element) => Card(
-                      child: Column(
-                        children: <Widget>[
-                          Image.asset('assets/food.jpg'),
-                          Text(element)
-                        ],
-                      ),
-                    ))
-                .toList(),
-          );
+    return ListView.builder(
+      itemBuilder: _buildProductItem,
+      itemCount: products.length,
+    );
   }
 }
