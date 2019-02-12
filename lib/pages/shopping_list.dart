cc4ca6fbf2ec952739d3dbedc8b0ad3cb9bbee13
tehnools <noelsoong@live.com>
2019-02-12 15:02:57 +1300

    3 new text fields created for shopping list

--- lib/pages/shopping_list.dart
+++ lib/pages/shopping_list.dart
@@ -1,8 +1,46 @@
 import 'package:flutter/material.dart';
 
-class ShoppingListPage extends StatelessWidget{
+class ShoppingListPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ShoppingListPageState();
+  }
+}
+
+class _ShoppingListPageState extends State<ShoppingListPage> {
+  String title = '';
+  String description = '';
+  double price;
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Shoping Lists'),);
+    return Column(
+      children: <Widget>[
+        TextField(
+          onChanged: (String value) {
+            setState(() {
+              title = value;
+            });
+          },
+        ),
+        TextField(
+          maxLines: 4,
+          onChanged: (String value) {
+            setState(() {
+              description = value;
+            });
+          },
+        ),
+        TextField(
+          keyboardType: TextInputType.number,
+          onChanged: (String value) {
+            setState(() {
+              price = double.parse(value);
+            });
+          },
+        ),
+        Text(title),
+      ],
+    );
   }
 }
